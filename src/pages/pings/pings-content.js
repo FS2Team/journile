d9e08ace0d84ccb7c39cf3771f657335fd068af2
mrsjiodev <51673347+mrsjiodev@users.noreply.github.com>
2020-07-04 01:41:36 +0700

    Revert "[add] - content pings"

--- src/pages/pings/pings-content.js
+++ src/pages/pings/pings-content.js
@@ -1,11 +1,6 @@
 /* @flow */
 
 import React from 'react'
-import './pings-content.scss'
-import AvtImg from '../../resources/image/avatar.png'
-import Icon from '../../components/icon'
-import Avatar from '../../components/avatar'
-import PingMessages from './ping-messages'
 
 type State = {}
 
@@ -15,28 +10,7 @@ class PingsContent extends React.Component<any, State>{
   render() {
     return (
       <div className="pings-content">
-        <div className="barContent">
-          <div className="user-info">
-            <div className="user-ctn">
-              <div className="avt">
-                {/* <img src={AvtImg} alt="" /> */}
-                <Avatar url={AvtImg} size={200} />
-              </div>
-              <div className="user-detail">
-                <h3>John Doe <span>@therealjohndoe</span></h3>
-                <p>Manhattan, U.S.A</p>
-              </div>
-            </div>
-          </div>
-          <div className="actions-call">
-            <Icon name="box-add" className="icon" size={24} />
-          </div>
-          <div className="right-options">
-            <p>Reading </p>
-          </div>
-        </div>
-
-        <PingMessages />
+        <h1>Pings COntent</h1>
       </div>
     )
   }
